ce79aa0b3fc13f9d74b3fabc0d435313f4ae2ff5
Aleksandr <keytolife94@gmail.com>
2024-06-26 20:52:53 -0500

    added questions, added README generate

--- assets/js/generateMarkdown.js
+++ assets/js/generateMarkdown.js
@@ -1,19 +1,84 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+  if (!license || license === 'None') {
+    return '';
+  }
+  return `![License: ${license}](https://img.shields.io/badge/License-${license}-blue.svg)`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  if (!license || license === 'None') {
+    return '';
+  }
+  switch (license) {
+    case 'MIT':
+      return 'https://choosealicense.com/licenses/mit/';
+    case 'GPLv3':
+      return 'https://choosealicense.com/licenses/gpl-3.0/';
+    case 'Apache-2.0':
+      return 'https://choosealicense.com/licenses/apache-2.0/';
+    default:
+      return '';
+  }
+}
+
+function renderLicenseContent(license) {
+  if (!license || license === 'None') {
+    return '';
+  } else {
+    return `\n- [License](#license)`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+  if (!license || license === 'None') {
+    return '';
+  }
+  return `## License
+This project is licensed under the [${license}](${renderLicenseLink(license)}) License.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)${renderLicenseContent(data.license)}
+- [Questions](#questions)
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+${renderLicenseSection(data.license)}
+
+## Questions
+If you have any questions about the project, feel free to contact me:
+
+- GitHub: [${data.githubUsername}](https://github.com/${data.githubUsername})
+- Email: [${data.email}](mailto:${data.email})
 `;
 }
 
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,60 @@
 // TODO: Include packages needed for this application
+const inquirer = require(`inquirer`);
+const fs = require(`fs`)
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        message: 'What is the name of your project?',
+        name: 'projectName'
+    },
+    {
+        message: 'Please provide a brief description of your project:',
+        name: 'description'
+    },
+    {
+        message: 'What are the installation instructions?',
+        name: 'installation'
+    },
+    {
+        message: 'How do you use your application?',
+        name: 'usage'
+    },
+    {
+        message: 'What are the contribution guidelines?',
+        name: 'contributing'
+    },
+    {
+        message: 'What are the test instructions?',
+        name: 'tests'
+    },
+    {
+        message: 'Choose a license for your project:',
+        name: 'license',
+        type: 'list',
+        choices: ['MIT', 'GPLv3', 'Apache-2.0', 'None']
+    },
+    {
+        message: 'What is your GitHub username?',
+        name: 'githubUsername'
+    },
+    {
+        message: 'What is your email address?',
+        name: 'email'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.error(err) : console.log('README created');
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
+
+inquirer.prompt(questions);
